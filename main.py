c655fe233f8a84199e1e335f220ff7a57c6c117d
Samson Adejoro <samsonadejoro@gmail.com>
2022-08-26 14:51:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+h = []
+with open('cat.txt', encoding='utf8') as f:
+    for line in f:
+        line.strip()
+        h.append(line)
+        # print(line.strip())
+print(h[2])
 
+with open("out.txt", "a") as file_object:
+    # Append 'hello' at the end of file
+    file_object.write("\n" + h[2])
